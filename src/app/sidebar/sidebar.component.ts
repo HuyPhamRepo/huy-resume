f2045b5de9cf5a953edb0ef680b90baed005c4d9
HStudyPersonal <study.quanghuy@gmail.com>
2018-07-21 01:51:00 +0800

    Removed sidebar component

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -4,19 +4,19 @@ import { trigger, state, style, animate, transition} from '@angular/animations';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css'],
-  animations: [
-    trigger('state', [
-      state('inactive', style({
-        width: '0px'
-      })),
-      state('active', style({
-        width: '200px'
-      })),
-      transition('inactive => active', animate('100ms ease-in')),
-      transition('active => inactive', animate('100ms ease-out'))
-    ])
-  ]
+  styleUrls: ['./sidebar.component.css']
+  //animations: [
+  //  trigger('state', [
+  //    state('inactive', style({
+  //      width: '0px'
+  //    })),
+  //    state('active', style({
+  //      width: '200px'
+  //    })),
+  //    transition('inactive => active', animate('100ms ease-in')),
+  //    transition('active => inactive', animate('100ms ease-out'))
+  //  ])
+  //]
 })
 
 export class SidebarComponent implements OnInit {
